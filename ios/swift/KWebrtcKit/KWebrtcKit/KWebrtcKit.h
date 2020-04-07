6bff54dfbd668d5e28a6c33a3def390a58bf7f34
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:15:59 +0800

    init project

---
+++ ios/swift/KWebrtcKit/KWebrtcKit/KWebrtcKit.h
@@ -0,0 +1,19 @@
+//
+//  KWebrtcKit.h
+//  KWebrtcKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KWebrtcKit.
+FOUNDATION_EXPORT double KWebrtcKitVersionNumber;
+
+//! Project version string for KWebrtcKit.
+FOUNDATION_EXPORT const unsigned char KWebrtcKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KWebrtcKit/PublicHeader.h>
+
+
